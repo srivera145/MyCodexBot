0b94e0214d87a33faf594c2dd2a34e33758a7233
Santos Rivera <srivera145@gmail.com>
2022-12-30 08:19:29 -0500

    update the server URL

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
                                 // https://codex-im0y.onrender.com/
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://mychatbot-1zld.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
